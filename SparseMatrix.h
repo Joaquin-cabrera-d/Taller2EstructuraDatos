1168bee7fbd280b91c98444cb9ab13875185f428
Joaquin-cabrera-d <joaquin.cabrera@alumnos.ucn.cl>
2025-10-24 16:32:53 -0300

    Agregado el metodo privado addInternal

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -6,11 +6,13 @@ class SparseMatrix {
 private:
     std::unordered_map<int, Nodo*> filaHeads;
     std::unordered_map<int, Nodo*> colHeads;
+    void addInternal(int X, int Y, int valor);
 public:
     SparseMatrix();
     SparseMatrix(const SparseMatrix& other); // generar copia
     SparseMatrix& operator=(const SparseMatrix& other);
     void add(int X, int Y, int valor);
+	void addRandom(int X, int Y, int valor);
     int get(int X, int Y);
     int remover(int X, int Y);
     void printStoredValues();
