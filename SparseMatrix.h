8c6d5f24415dbba91762446f215e2bec73dc556f
Joaquin-cabrera-d <joaquin.cabrera@alumnos.ucn.cl>
2025-10-23 21:40:25 -0300

    Añadida una funcion para guardar la matriz A si se genera automaticamente

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -8,6 +8,8 @@ private:
     std::unordered_map<int, Nodo*> colHeads;
 public:
     SparseMatrix();
+    SparseMatrix(const SparseMatrix& other); // generar copia
+    SparseMatrix& operator=(const SparseMatrix& other);
     void add(int X, int Y, int valor);
     int get(int X, int Y);
     int remover(int X, int Y);
